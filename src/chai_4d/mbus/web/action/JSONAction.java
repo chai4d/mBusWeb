b81a169bcba4ef76f9e99767e11a79f28a6a8155
Chai <Chai@Chai-PC>
2016-04-16 16:35:15 +0700

    No need WebSession for JSON Called

--- src/chai_4d/mbus/web/action/JSONAction.java
+++ src/chai_4d/mbus/web/action/JSONAction.java
@@ -13,35 +13,23 @@ public class JSONAction extends BaseAction
     private static final long serialVersionUID = -2804891508299782800L;
     private static final Logger log = LogManager.getLogger(JSONAction.class);
 
-    public static final String sessionKey = "json.pointNames";
-
     private List<PointName> pointNames;
 
-    @SuppressWarnings("unchecked")
     public String doExecute() throws Exception
     {
         log.debug("In execute method");
 
-        if (sessionMap.containsKey(sessionKey))
-        {
-            pointNames = (List<PointName>) sessionMap.get(sessionKey);
-        }
-        else
-        {
-            pointNames = HomeService.getPointNames("en");
+        pointNames = HomeService.getPointNames("en");
 
-            // TODO -- put some mock up data
-            if (pointNames.size() == 0)
-            {
-                pointNames.add(new PointName(1, "Name-1", "Name-1"));
-                pointNames.add(new PointName(2, "Name-2", "Name-2"));
-                pointNames.add(new PointName(3, "Name-3", "Name-3"));
-                pointNames.add(new PointName(4, "Name-4", "Name-4"));
-                pointNames.add(new PointName(5, "Name-5", "Name-5"));
-                pointNames.add(new PointName(6, "Name-6", "Name-6"));
-            }
-
-            sessionMap.put(sessionKey, pointNames);
+        // TODO -- put some mock up data
+        if (pointNames.size() == 0)
+        {
+            pointNames.add(new PointName(1, "Name-1", "Name-1"));
+            pointNames.add(new PointName(2, "Name-2", "Name-2"));
+            pointNames.add(new PointName(3, "Name-3", "Name-3"));
+            pointNames.add(new PointName(4, "Name-4", "Name-4"));
+            pointNames.add(new PointName(5, "Name-5", "Name-5"));
+            pointNames.add(new PointName(6, "Name-6", "Name-6"));
         }
 
         return SUCCESS;
